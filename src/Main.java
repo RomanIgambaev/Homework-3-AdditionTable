3d2e9b459d60685a4dedba250257ebd95e00f8b1
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-19 18:27:27 +0500

    Addition

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number = 0;
+        for (int counter = 1; counter <= 9; counter++) {
+            number++;
+            if (number == 1) {
+                int counter2 = 0;
+                while (counter2 <= 8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2 + number));
+                }
+            } else if (number == 2) {
+                int counter2 = 0;
+                while (counter2<=8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2+number));
+                }
+            } else if (number == 3) {
+                int counter2 = 0;
+                while (counter2<=8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2+number));
+                }
+            } else if (number == 4) {
+                int counter2 = 0;
+                while (counter2<=8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2+number));
+                }
+            } else if (number == 5) {
+                int counter2 = 0;
+                while (counter2<=8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2+number));
+                }
+            } else if (number == 6) {
+                int counter2 = 0;
+                while (counter2<=8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2+number));
+                }
+            } else if (number == 7) {
+                int counter2 = 0;
+                while (counter2<=8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2+number));
+                }
+            } else if (number == 8) {
+                int counter2 = 0;
+                while (counter2<=8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2+number));
+                }
+            } else if (number == 9) {
+                int counter2 = 0;
+                while (counter2<=8) {
+                    counter2++;
+                    System.out.println(number + "+" + counter2 + "=" + (counter2+number));
+                }
+            }
+        }
     }
 }
